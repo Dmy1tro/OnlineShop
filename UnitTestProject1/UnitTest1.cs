c86500538c6c22ab943549863793013cb8db4385
dmytro.laskuryk <dmytro.laskuryk@nure.ua>
2019-07-18 22:27:50 +0300

    del

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OnlineShop.Controllers;
 using OnlineShop.Abstract;
@@ -6,6 +6,7 @@ using OnlineShop.Concrete;
 using Moq;
 using System.Collections.Generic;
 using OnlineShop;
+using OnlineShop.Infrastructure;
 
 namespace UnitTestProject1
 {
@@ -13,17 +14,31 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Add_range_cart()
         {
             Mock<IBookDbRepository> mock = new Mock<IBookDbRepository>();
-            mock.Setup(x => x.Genres as IEnumerable<Genre>).Returns(new List<Genre>
+            mock.Setup(x => x.Books as IEnumerable<Book>).Returns(new List<Book>
             {
-                new Genre { GenreName = "genre1", Books = new List<Book> { new Book { BookId = 1 } } },
-
+                new Book{ BookId= 1, Name = "Name1" },
+                new Book{ BookId= 1, Name = "Name1" },
+                new Book{ BookId= 2, Name = "Name2" },
+                new Book{ BookId= 2, Name = "Name2" },
+                new Book{ BookId= 2, Name = "Name2" },
+                new Book{ BookId= 3, Name = "Name3" },
+                new Book{ BookId= 3, Name = "Name3" },
+                new Book{ BookId= 4, Name = "Name4" },
+                new Book{ BookId= 5, Name = "Name5" },
             });
-            EFBookRepository repository = new EFBookRepository();
-            var result = repository.GetGenresCount();
-            Assert.AreEqual(1,1);
+
+            Cart cart = new Cart();
+            CartController controller = new CartController(mock.Object);
+
+            controller.AddToCart(cart, 1);
+            controller.AddToCart(cart, 2);
+
+            Assert.AreEqual(cart.TotalCart.Count(), 2);
+            Assert.AreEqual(cart.TotalCart.FirstOrDefault(x => x.Book.BookId == 1).Amount, 1);
+            Assert.AreEqual(cart.TotalCart.FirstOrDefault(x => x.Book.BookId == 2).Amount, 1);
         }
     }
 }
